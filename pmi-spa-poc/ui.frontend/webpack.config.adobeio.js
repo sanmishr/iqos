61b1f070b547895c2cfb31b829decae1f2fd5352
Michal Goszczynski <goszczyn@adobe.com>
2022-12-08 16:45:08 +0100

    fixing webpack build for adobe io app and and SSR
    * fixing webpack build for adobe io app.
    
    * upgrade to node 16 and npm 8
    
    * update aiocli, remove ootb example test to reduce error vector during poc
    
    * upgrade non critical packages - part 1
    
    * fixes for webpack, updating node dependencies and bumping ioruntime to node16
    
    Co-authored-by: Luca Nerlich <luca.nerlich@googlemail.com>

--- pmi-spa-poc/ui.frontend/webpack.config.adobeio.js
+++ pmi-spa-poc/ui.frontend/webpack.config.adobeio.js
@@ -18,7 +18,7 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const WebpackShellPlugin = require('webpack-shell-plugin');
 var isTestEnvironment = process.env.NODE_ENV == 'test';
 
@@ -53,7 +53,7 @@ const serverConfig = {
     devtool: 'source-map',
 
     resolve: {
-        extensions: ['.js', 'jsx'],
+        extensions: ['.js', '.jsx','.tsx', '.ts'],
         // This allows you to set a fallback for where Webpack should look for modules.
         // We placed these paths second because we want `node_modules` to "win"
         // if there are any conflicts. This matches Node resolution mechanism.
@@ -64,14 +64,15 @@ const serverConfig = {
     module: {
         rules: [
             {
-                test: /\.jsx?$/,
+                test: /\.(j|t)sx?$/,
                 enforce: 'post',
                 loader: require.resolve('babel-loader'),
                 options: {
                     babelrc: false,
                     presets: [
                         ['@babel/preset-env'],
-                        ['@babel/react']
+                        ['@babel/react'],
+                        ['@babel/preset-typescript']
                     ],
                     plugins: [
                         ['universal-import']
@@ -106,7 +107,7 @@ const serverConfig = {
             "API_HOST": process.env.API_HOST,
             "APP_ROOT_PATH": process.env.APP_ROOT_PATH
         }),
-        new CleanWebpackPlugin(['dist']),
+        new CleanWebpackPlugin(),
         new WebpackShellPlugin({onBuildEnd:['node ./scripts/postWebhook.js']}),
         // Output a single chunk at most to make sure all code is loaded on
         // the server side.
